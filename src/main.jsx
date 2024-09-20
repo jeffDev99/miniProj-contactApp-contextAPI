5bff0f433538c34104118799c008b8b93169892f
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-09-20 22:47:52 +0330

    install axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from "react";
+import { StrictMode , createContext } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./var.css";
 import "./global.css";
+export const contactContext = createContext()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <contactContext.Provider>
       <App />
+    </contactContext.Provider>
     </BrowserRouter>
   </StrictMode>
 );
