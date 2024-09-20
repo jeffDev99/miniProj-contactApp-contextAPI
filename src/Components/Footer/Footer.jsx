bf456a8438258e6ff43d8b5a04412be0c1a19ca8
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-09-20 21:20:39 +0330

    compelete footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className={styles.footer}>
+      <p>
+        Code With ❤️ | <a href="https://github.com/jeffDev99">@JEFFDEV99</a>
+      </p>
+    </footer>
+  );
 }
