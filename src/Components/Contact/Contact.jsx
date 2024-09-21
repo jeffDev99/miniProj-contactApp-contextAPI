54452a28d0d8992799b4ab888405435b54854ccb
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-09-21 11:14:33 +0330

    debug contact list

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { contactContext } from "../../main";
+import Modal from "../Modal/Modal";
+import styles from "./Contact.module.css";
 
 export default function Contact() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [contactToDelete, setContactToDelete] = useState(null);
+  const [isShowBtns, setIsShowBtns] = useState(false);
+
+  const { contacts } = useContext(contactContext);
+
+  const filteredContacts = contacts.filter((contact) => contact.fullname.toLowerCase().includes(searchQuery.toLowerCase()) || contact.email.toLowerCase().includes(searchQuery.toLowerCase()));
+
+  const openModal = (contact) => {
+    setContactToDelete(contact);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleDelete = () => {
+    deleteHandler(contactToDelete.id);
+    setModalOpen(false);
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <>
+      <h4 className={styles.subtitle}>{contacts.length} TOTAL</h4>
+      <h2 className={styles.title}>Contacts</h2>
+      <input type="text" placeholder="Search by name or email" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className={styles.formSearch} style={{ marginBottom: "1rem" }} />
+      <div style={{ overflowX: "auto" }}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Full Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th style={{ width: "120px", alignItems: "center" }}>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredContacts.length ? (
+              filteredContacts.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.fullname}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td style={{ display: "flex" ,justifyContent:"center", flexDirection: isShowBtns ? "normal" : "center" }}>
+                    {isShowBtns ? (
+                      <>
+                        <button className="btn btn-danger" style={{ marginBottom: ".5rem" }} onClick={() => deleteHandler(item)}>
+                          Delete 🗑
+                        </button>
+                        <button className="btn btn-primary" onClick={() => startEditing(item)}>
+                          Edit ✏️
+                        </button>
+                      </>
+                    ) : (
+                      <img src="/images/more.png" className={styles.contactIcon} onClick={() => setIsShowBtns(true)} />
+                    )}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={6}>
+                  <p className="alert alert-primary">No contact yet</p>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Modal */}
+      <Modal isOpen={isModalOpen} onClose={closeModal} title="Delete Contact">
+        <p>Are you sure you want to delete {contactToDelete?.fullname}?</p>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "1rem",
+          }}
+        >
+          <button onClick={handleDelete} className="btn btn-primary" style={{ marginRight: ".5rem" }}>
+            Yes, delete
+          </button>
+          <button onClick={closeModal} className="btn btn-light">
+            Cancel
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,35 @@
-import { StrictMode , createContext } from "react";
+import { StrictMode, createContext, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { api } from "./Services/config.js";
+import App from "./App.jsx";
 import "./var.css";
 import "./global.css";
-export const contactContext = createContext()
+
+export const contactContext = createContext();
+
+const Main = () => {
+  const [contacts, setContacts] = useState([]);
+  const [contact, setContact] = useState([]);
+
+  useEffect(() => {
+    api.get("/contacts").then(res => {
+      console.log(res);
+      setContacts(res.data); 
+    });
+  }, []);
+
+  return (
+    <contactContext.Provider value={{contacts , contact , setContact}}>
+      <App />
+    </contactContext.Provider>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <contactContext.Provider>
-      <App />
-    </contactContext.Provider>
+      <Main />
     </BrowserRouter>
   </StrictMode>
 );
